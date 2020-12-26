93d50b6f0a68bec23b7590a0e2e26ce0916354a1
magacula <makoigacula@gmail.com>
2020-12-26 02:53:32 -0800

    highlight active scrolling functionality finished

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-// function that displays / hides (toggles) mobile menu
+// Global variables of the elements we want to target
+const nav = document.querySelector(".nav-toggle");
+const menu = document.querySelector(".nav__list");
+const navLink = document.querySelectorAll(".nav__link");
+const navLogo = document.getElementById("logo");
+const sections = document.querySelectorAll("section[id]");
+
+/*===== Display / hide (toggles) mobile menu =====*/
 const mobileMenu = () => {
-  const nav = document.querySelector(".nav-toggle");
-  const menu = document.querySelector(".nav__list");
   // Event listener that triggers upon click event
   nav.addEventListener("click", () => {
     console.log("clicked");
@@ -11,6 +16,49 @@ const mobileMenu = () => {
   });
 };
 
-mobileMenu();
+/*===== Remove Menu upon clicking on link  =====*/
+const hideMenu = () => {
+  // remove menu if logo is clicked
+  navLogo.addEventListener("click", () => {
+    menu.classList.remove("nav-active");
+  });
 
-function app() {}
+  // remove menu if one of the links are clicked
+  navLink.forEach((n) =>
+    n.addEventListener("click", () => {
+      // remove active menu
+      menu.classList.remove("nav-active");
+    })
+  );
+};
+
+/*===== Display active bar on scrolled section =====*/
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute("id");
+    console.log(`.nav__list .nav__item a[href='#${sectionId}']`);
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(`.nav__list .nav__item a[href='#${sectionId}']`)
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(`.nav__list .nav__item a[href='#${sectionId}']`)
+        .classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
+// Runs all functions
+const app = () => {
+  mobileMenu();
+  hideMenu();
+  scrollActive();
+};
+app();
